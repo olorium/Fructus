b555b01f544b3620c9e14562c3ae79f090caae48
Alex Vasyliev <olorin2007@gmail.com>
2023-04-07 14:28:45 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Oleksii Vasyliev on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
